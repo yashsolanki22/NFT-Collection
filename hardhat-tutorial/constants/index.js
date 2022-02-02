12bb4e1340e174c0c611f58af1e1e163597c400d
Yash solanki <47449493+yash-solanki069@users.noreply.github.com>
2022-02-02 23:34:47 +0530

    Add files via upload

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xf4CA556A39C41259291C4667feB77465C93b74d2";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-lovat.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
